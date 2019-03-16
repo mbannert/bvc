ff1f0944eab5ce6dce39c10eb4c1efee98333965
mbannert <matthias.bannert@gmail.com>
2019-03-16 23:44:53 +0100

    working to generate an overview

--- generate_ovw_and_prof.R
+++ generate_ovw_and_prof.R
@@ -29,5 +29,70 @@ addClass <- function(s, tr_class = NULL, td_class = NULL){
 } 
 
 
+courts %>% 
+  filter(Name == a) %>% 
+  select(latitude, longitude)
+
+
+
+
+replace_table <- function(tr_element, temp){
+  gsub("####_TABLE_####",tr_element,temp)
+}
+
+replace_coords <- function(fn, file, df){
+  element <- df %>% 
+    filter(file_name == fn) %>% 
+    select(longitude, latitude)
+  
+  file <- gsub("####_LONG_####", element$longitude, file)
+  gsub("####_LAT_####", element$latitude, file)
+}
+
+
+# call functions, generate court profiles :)
+# read in a template file in order to replace the placeholders :)
+template <- paste(readLines("bvc/courts/court_template.html"),
+                  collapse = "\n")
 ll <- lapply(split(courts,courts$Name),wrwr,nms = names(courts))
+ll_collapsed <- lapply(ll,function(x) paste(x,collapse="\n"))
+html_files <- lapply(ll_collapsed, replace_table, temp = template)
+
+# replace umlauts and other ugly chars 
+# to be able to generate nice file names
+names(html_files) <- tolower(gsub(" |\\.\\'","_",names(html_files)))
+names(html_files) <- gsub("ö","oe",names(html_files))
+names(html_files) <- gsub("ä","ae",names(html_files))
+names(html_files) <- gsub("ü","ue",names(html_files))
+
+# add file name to initial tibble 
+courts$file_name <- names(html_files)
+
+html_files <- lapply(names(html_files), function(x){
+  replace_coords(x, html_files[[x]], courts)
+})
+
+names(html_files) <- courts$file_name
+
+
+lapply(names(html_files), function(x) {
+  cat(html_files[[x]],file = sprintf("bvc/courts/%s.html",x))  
+})
+
+
+# generate an overview table
+courts$link <- sprintf("courts/%s.html", courts$file_name)
+
+
+overview <- courts %>% 
+  select(id, Name, link, Courts, Kategorie, longitude, latitude)
+
+display <- overview %>% 
+  select(Name, Courts, Kategorie)
+
+xx <- knitr::kable(display,"html")
+
+
+cat(xx,file = "test.html")
+
 
